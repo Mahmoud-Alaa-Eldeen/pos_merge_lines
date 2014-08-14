9bb30644f44616ee82d34471e9800bc0438fd048
agb80 <atin81@gmail.com>
2014-08-14 10:32:04 -0500

    Fixed problem mergin different category units

--- static/src/js/models.js
+++ static/src/js/models.js
@@ -41,6 +41,26 @@ function openerp_merge_lines(instance, module){
             this.get('orderLines').add(line);
             this.selectLine(this.getLastOrderline());
         },
-	});
+    });
     
+    module.MergeLinesOrderline = module.Orderline;
+    module.Orderline = module.MergeLinesOrderline.extend({
+        // when we add an new orderline we want to merge it with other lines to see reduce the number of items
+        // in the orderline. This returns true if it makes sense to merge the two
+        // TODO: this function overrides default point of sale behavior on Odoo that
+        // prevents merge items in other units than pieces 
+        can_be_merged_with: function(orderline){
+            if( this.get_product().id !== orderline.get_product().id){    //only orderline of the same product can be merged
+                return false;
+            }else if(this.get_product_type() !== orderline.get_product_type()){
+                return false;
+            }else if(this.get_discount() > 0){             // we don't merge discounted orderlines
+                return false;
+            }else if(this.price !== orderline.price){
+                return false;
+            }else{ 
+                return true;
+            }
+        },        
+    });
 }
